497a67637cefbe8439443a80637f959a7bf492a4
Vanessa Henson <ourmagicallife@gmail.com>
2015-12-12 12:50:43 -0700

    changed some image paths

--- portfollio.php
+++ portfollio.php
@@ -31,14 +31,14 @@ include('includes/header.php'); ?>
 			  		
 			  			<div class="col-xs-6 hidden-sm col-md-3">
 			  	  		<div class="codeExample">
-			  				<img src="/img/postIt.png" alt="Vanessa Henson">
+			  				<img src="img/postIt.png" alt="Vanessa Henson">
 			  				<div class="post-text">
 			  				<a href="https://github.com/webwolf2112/codeExamples/blob/master/stickySlider.js" target="blank">
 			  					<p class="post-heading">Responsive JQuery</p>
 			  					<p class="post-body">Sticky Slider</p>
 			  					</a>
 			  					<div class="line">
-			  					<img src="/img/line.png" class="img-responsive" alt="vanessa henson">
+			  					<img src="img/line.png" class="img-responsive" alt="vanessa henson">
 			  					<p class="post-bottom"><a href="http://www.coloradohazard.com/course-descriptions.php" target="blank">View On Live Site</a></p>
 			  					</div>
 			  				</div>
@@ -48,12 +48,12 @@ include('includes/header.php'); ?>
 			  		
 			  		<div class="col-xs-6 col-sm-4 col-md-3">
 			  	  		<div class="codeExample">
-			  				<a href="https://github.com/webwolf2112/codeExamples" target="blank"><img src="/img/postIt.png" alt="Vanessa Henson">
+			  				<a href="https://github.com/webwolf2112/codeExamples" target="blank"><img src="img/postIt.png" alt="Vanessa Henson">
 			  				<div class="post-text">
 			  					<p class="post-heading">Visit My</p>
 			  					<p class="post-body">Git Hub Repo</p>
 			  					<div class="line">
-			  					<img src="/img/line.png" class="img-responsive" alt="vanessa henson">
+			  					<img src="img/line.png" class="img-responsive" alt="vanessa henson">
 			  					</div>
 			  				</div>
 			  				</a>
@@ -62,46 +62,46 @@ include('includes/header.php'); ?>
 			  		
 			  		<div class="col-xs-6 col-sm-4 col-md-3">
 			  	  		<div class="codeExample">
-			  				<a class="lightbox" href="/img/1940sGirlLarge.png" data-lightbox-gallery="gallery1"><img src="/img/postItPink.png" alt="Vanessa Henson">
+			  				<a class="lightbox" href="img/1940sGirlLarge.png" data-lightbox-gallery="gallery1"><img src="img/postItPink.png" alt="Vanessa Henson">
 			  				<div class="post-text no-heading">
 			  					<p class="post-body">Graphics<br/> Gallery!</p>
 			  					<div class="line">
-			  					<img src="/img/line.png" class="img-responsive" alt="vanessa henson">
+			  					<img src="img/line.png" class="img-responsive" alt="vanessa henson">
 			  					</div>
 			  				</div>
 			  				</a>
 			  						<!-- gallery links --> 
-								<a class="lightbox" href="/img/naturalBeautyLarge.png" data-lightbox-gallery="gallery1"></a>
-								<a class="lightbox" href="/img/steampunkRavenLarge.png" data-lightbox-gallery="gallery1"></a>
-							  	<a class="lightbox" href="/img/treeLarge.png" data-lightbox-gallery="gallery1"></a>
-							   	<a class="lightbox" href="/img/layoffShieldLarge.png" data-lightbox-gallery="gallery1"></a>
-							   	<a class="lightbox" href="/img/yingYang.png" data-lightbox-gallery="gallery1"></a>
+								<a class="lightbox" href="img/naturalBeautyLarge.png" data-lightbox-gallery="gallery1"></a>
+								<a class="lightbox" href="img/steampunkRavenLarge.png" data-lightbox-gallery="gallery1"></a>
+							  	<a class="lightbox" href="img/treeLarge.png" data-lightbox-gallery="gallery1"></a>
+							   	<a class="lightbox" href="img/layoffShieldLarge.png" data-lightbox-gallery="gallery1"></a>
+							   	<a class="lightbox" href="img/yingYang.png" data-lightbox-gallery="gallery1"></a>
 							   	<!-- end gallery links -->
 			  			</div>
 			  		</div>
 			  		
 			  			<div class="col-xs-6 col-sm-4 col-md-3">
 			  	  		<div class="codeExample">
-			  				<a class="lightbox" href="/img/banners/axiom.png" data-lightbox-gallery="gallery3"><img src="/img/postItPink.png" alt="Vanessa Henson">
+			  				<a class="lightbox" href="img/banners/axiom.png" data-lightbox-gallery="gallery3"><img src="img/postItPink.png" alt="Vanessa Henson">
 					  				<div class="post-text no-heading"> 
 					  					<p class="post-body">Banners</p> 
 					  					<div class="line-height-adjust"> & 
 					  					</div>  
 					  					<p class="post-body">App Icons</p>
 					  					<div class="line">
-					  						<img src="/img/line.png" class="img-responsive" alt="vanessa henson">
+					  						<img src="img/line.png" class="img-responsive" alt="vanessa henson">
 					  					</div>
 			  						</div>
 			  				</a>
 			  				<!-- banner  links -->
-								<a class="lightbox" href="/img/banners/1.png" data-lightbox-gallery="gallery3"></a>
-								<a class="lightbox" href="/img/banners/2.png" data-lightbox-gallery="gallery3"></a>
-								<a class="lightbox" href="/img/banners/3.png" data-lightbox-gallery="gallery3"></a>
-								<a class="lightbox" href="/img/banners/4.png" data-lightbox-gallery="gallery3"></a>
-								<a class="lightbox" href="/img/banners/5.png" data-lightbox-gallery="gallery3"></a>
-								<a class="lightbox" href="/img/banners/6.png" data-lightbox-gallery="gallery3"></a>
-								<a class="lightbox" href="/img/banners/7.png" data-lightbox-gallery="gallery3">	</a>
-								<a class="lightbox" href="/img/banners/katy.png" data-lightbox-gallery="gallery3"></a>
+								<a class="lightbox" href="img/banners/1.png" data-lightbox-gallery="gallery3"></a>
+								<a class="lightbox" href="img/banners/2.png" data-lightbox-gallery="gallery3"></a>
+								<a class="lightbox" href="img/banners/3.png" data-lightbox-gallery="gallery3"></a>
+								<a class="lightbox" href="img/banners/4.png" data-lightbox-gallery="gallery3"></a>
+								<a class="lightbox" href="img/banners/5.png" data-lightbox-gallery="gallery3"></a>
+								<a class="lightbox" href="img/banners/6.png" data-lightbox-gallery="gallery3"></a>
+								<a class="lightbox" href="img/banners/7.png" data-lightbox-gallery="gallery3">	</a>
+								<a class="lightbox" href="img/banners/katy.png" data-lightbox-gallery="gallery3"></a>
 								<!-- end banner  links -->
 			  			</div>
 			  		</div>
